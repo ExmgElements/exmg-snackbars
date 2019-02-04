98777a81029bd92d52c31b3ad5ab05d9e6d7404c
uladzislau <uladzislau.zayats@gmail.com>
2019-02-04 11:40:59 +0100

    PTWO-215 Snackbars

---
+++ exmg-snackbars.ts
@@ -0,0 +1,16 @@
+import {LitElement, html, customElement} from 'lit-element';
+
+import '@polymer/paper-icon-button/paper-icon-button';
+
+import '@polymer/iron-flex-layout/iron-flex-layout';
+import '@polymer/paper-styles/paper-styles';
+
+@customElement('exmg-snackbars')
+export class ExmgSnackbars extends LitElement {
+
+  protected render() {
+    return html`
+    zzz
+    `;
+  }
+}
