af06bd04b9fdee88bd908c0e95a8c110de46c28e
uladzislau <uladzislau.zayats@gmail.com>
2019-02-04 15:30:33 +0100

    PTWO-215 Snackbars

--- exmg-snackbars.ts
+++ exmg-snackbars.ts
@@ -1,22 +1,36 @@
-import {LitElement, html, customElement, PropertyValues} from 'lit-element';
-
 import '@polymer/paper-icon-button/paper-icon-button';
 
-import '@polymer/iron-flex-layout/iron-flex-layout';
-import '@polymer/paper-styles/paper-styles';
+import '@polymer/paper-toast';
 
-import {MDCSnackbar} from '@material/snackbar/index';
+function _getToastContainerNode(): Node {
+  const toastContainerNodeId = 'paper-toast-container';
 
-@customElement('exmg-snackbars')
-export class ExmgSnackbars extends LitElement {
-  protected firstUpdated(_changedProperties: PropertyValues): void {
-    // const snackbar = new MDCSnackbar(this.shadowRoot!.querySelector('.mdc-snackbar'));
+  const existingNode = document.querySelector('#' + toastContainerNodeId);
+
+  if (existingNode) {
+    return existingNode;
   }
 
-  // protected render() {
-  //   return html`
-  //   <link rel="stylesheet" href="/node_modules/@material/snackbar/dist/mdc.snackbar.css" />
-  //   <div class="mdc-snackbar mdc-snackbar--open"><div class="mdc-snackbar__surface"><div class="mdc-snackbar__label" role="status" aria-live="polite">Can't send photo. Retry in 5 seconds.</div><div class="mdc-snackbar__actions"><button type="button" class="mdc-button mdc-snackbar__action">Retry</button><button class="mdc-icon-button mdc-snackbar__dismiss material-icons" title="Dismiss">close</button></div></div></div>
-  //   `;
-  // }
+  let toastContainerNode = document.createElement('div');
+  toastContainerNode.id = toastContainerNodeId;
+  document.body.appendChild(toastContainerNode);
+
+  return toastContainerNode;
+}
+
+export function showSnackBar(message: string) {
+  const toastContainerNode = _getToastContainerNode();
+
+  let toastNode = document.createElement('paper-toast');
+  toastContainerNode.appendChild(toastNode);
+
+  toastNode.text = message;
+  toastNode.opened = true;
+
+  (new MutationObserver((_mutationsList: MutationRecord[], observer: MutationObserver) => {
+    if (!toastNode.opened) {
+      observer.disconnect();
+      toastContainerNode.removeChild(toastNode);
+    }
+  })).observe(toastNode, { attributes: true });
 }
