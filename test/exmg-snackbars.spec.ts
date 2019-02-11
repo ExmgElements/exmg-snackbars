2d991e3df12fca516deb559fe5d70c4f79ceae6d
uladzislau <uladzislau.zayats@amsterdam-standard.pl>
2019-02-11 13:20:34 +0100

    PTWO-215 Snackbars

--- test/exmg-snackbars.spec.ts
+++ test/exmg-snackbars.spec.ts
@@ -1,55 +1,49 @@
 import {showSnackBar} from '../exmg-snackbars';
 
 const {assert} = chai;
+const wait = (time: number) => new Promise(resolve => setTimeout(resolve, time));
 
 suite('<exmg-snackbars>', function () {
   suite('base usage', function () {
-    test('normal toast is displayed', function (done) {
+    test('normal toast is displayed', async function () {
       showSnackBar('test message', {duration: 1000});
 
       assert.exists(document.querySelector('#paper-toast-container'), 'Toast container should exist');
       assert.exists(document.querySelector('paper-toast'), 'Toast should exist');
       assert.notExists(document.querySelector('paper-icon-button'), 'Toast close button should exist');
 
-      setTimeout(() => {
-        assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
-        done();
-      },  1000);
+      await wait(1000);
+      assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
     });
 
-    test('normal toast with custom node id is displayed', function (done) {
+    test('normal toast with custom node id is displayed', async function () {
       showSnackBar('test message', {duration: 1000, toastContainerNodeId: 'custom-id'});
 
       assert.exists(document.querySelector('#' + 'custom-id'), 'Toast container should exist');
       assert.exists(document.querySelector('paper-toast'), 'Toast should exist');
       assert.notExists(document.querySelector('paper-icon-button'), 'Toast close button should exist');
 
-      setTimeout(() => {
-        assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
-        done();
-      },  1000);
+      await wait(1000);
+      assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
     });
 
-    test('infinite toast with close button is displayed', function (done) {
+    test('infinite toast with close button is displayed', async function () {
       showSnackBar('test message', {duration: 0, showCloseButton: true});
 
       assert.exists(document.querySelector('#paper-toast-container'), 'Toast container should exist');
       assert.exists(document.querySelector('paper-toast'), 'Toast should exist');
       assert.exists(document.querySelector('paper-icon-button'), 'Toast close button should exist');
 
-      setTimeout(() => {
-        assert.exists(document.querySelector('paper-toast'), 'Toast should still exist');
+      await wait(4000);
 
-        /**
-         * Testing toast close feature
-         */
-        document.querySelector('paper-icon-button')!.click();
-        setTimeout(() => {
-          assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
-          done();
-        }, 100);
+      /**
+       * Testing toast close feature
+       */
+      document.querySelector('paper-icon-button')!.click();
 
-      }, 4000);
+      await wait(100);
+
+      assert.notExists(document.querySelector('paper-toast'), 'Toast should not exist');
     });
   });
 });
